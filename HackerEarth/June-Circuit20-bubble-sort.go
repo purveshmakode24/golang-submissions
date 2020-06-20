f43740c41e0972bc2e4eab07f6738bfa7316118f
Purvesh Makode <makodepurvesh@gmail.com>
2020-06-21 00:27:11 +0530

    June circuit'20 bubble sort

---
+++ HackerEarth/June-Circuit20-bubble-sort.go
@@ -0,0 +1,37 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main() {
+	var n, a, temp int
+	var arr[] int
+	
+	fmt.Scan(&n)
+
+	for i:=0; i<n; i++ {
+		fmt.Scan(&a)
+		arr = append(arr, a)
+	}
+
+	cnt := 0
+
+	swapped := true
+
+	for swapped != false {
+		swapped = false
+		cnt = cnt + 1
+		
+		for i:=0; i<n-1; i++ {
+			if arr[i]>arr[i+1] {
+				temp = arr[i+1]
+            	arr[i+1] = arr[i]
+            	arr[i] = temp
+            	swapped = true
+			}
+		}
+	}
+
+	fmt.Print(cnt)
+}
